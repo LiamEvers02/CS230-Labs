b6e0dbc797e382efe8200d0c05c541134c805260
Liam Evers <you@example.com>
2024-03-20 01:10:21 +1300

    MySimpleRectangle
    java file containing code for MySimpleRectangle

---
+++ MySimpleRectangle.java
@@ -0,0 +1,13 @@
+import java.awt.Point;
+class MySimpleRectangle{
+    private Point topLeft;
+    private int width, height;
+    public MySimpleRectangle(){this.topLeft = new Point(0,0); this.width = 1; this.height = 2;}
+    public MySimpleRectangle(int x, int y){this.topLeft = new Point(x, y); this.width = 1; this.height = 2;}
+    public Point getTopLeft(){return this.topLeft;}
+    public int getWidth(){return this.width;}
+    public int getHeight(){return this.height;}
+    public int getPerimeter(){return (this.width * 2) + (this.height * 2);}
+    public int getArea(){return this.width * this.height;}
+    public String toString(){return String.format("%s, area=%d", getClass().getName(), getArea());}
+}
