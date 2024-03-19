3b68bb0f90013a244437fb88298a21365b11f202
Liam Evers <you@example.com>
2024-03-20 01:17:55 +1300

    Move folders
    Moved mySimpleRectangle to Lab04, fixed constructor issues, created MySimpleCuboid file aswell

--- Lab04/MySimpleRectangle.java
+++ Lab04/MySimpleRectangle.java
@@ -3,7 +3,8 @@ class MySimpleRectangle{
     private Point topLeft;
     private int width, height;
     public MySimpleRectangle(){this.topLeft = new Point(0,0); this.width = 1; this.height = 2;}
-    public MySimpleRectangle(int x, int y){this.topLeft = new Point(x, y); this.width = 1; this.height = 2;}
+    public MySimpleRectangle(int x, int y, int width, int height){this.topLeft = new Point(x,y); this.width = width; this.height = height;}
+    public MySimpleRectangle(Point topLeft){this.topLeft = topLeft; this.width = 1; this.height = 2;}
     public Point getTopLeft(){return this.topLeft;}
     public int getWidth(){return this.width;}
     public int getHeight(){return this.height;}
